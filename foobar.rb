0f7d3d2e3b719afd6ffe4fc410a0a10570c621bd
Justin Huang <justin-huang@berkeley.edu>
2017-09-15 10:37:29 -0700

    Fa17 Rails-decal-h1

--- foobar.rb
+++ foobar.rb
@@ -3,7 +3,16 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+    arr = a.map{|e| e.to_i + 2}.uniq.map{|e| e if e % 2 == 0}.compact.map{|e| e if e < 10}.compact
+    sum = 0
+    arr.each do |x|
+      sum += x
+    end
+    return sum
+
   end
 end
+
+Foobar.baz(['1', '1', '1', '2','3'])
 
 
--- hw1.rb
+++ hw1.rb
@@ -1,15 +1,18 @@
 def squared_sum(a, b)
   # Q1 CODE HERE
+  (a+b) * (a+b)
 
 end
 
 def sort_array_plus_one(a)
   # Q2 CODE HERE
+  a.map{|e| e + 1}.sort!
 
 end
 
 def combine_name(first_name, last_name)
   # Q3 CODE HERE
+  first_name + " " + last_name
 
 end
 
